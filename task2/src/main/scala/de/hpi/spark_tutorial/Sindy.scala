5d6fbfcab043e4ac4016772d794504f39f9a707c
Oliver Clasen <oliver.clasen@alice-dsl.net>
2018-12-19 17:48:18 +0100

    finished task and renamed

--- task2/src/main/scala/de/hpi/spark_tutorial/Sindy.scala
+++ task2/src/main/scala/de/hpi/spark_tutorial/Sindy.scala
@@ -1,10 +1,50 @@
 package de.hpi.spark_tutorial
 
-import org.apache.spark.sql.SparkSession
+import org.apache.spark.rdd.RDD
+import org.apache.spark.sql.{DataFrame, SparkSession}
+
+import scala.collection.mutable.ListBuffer
 
 object Sindy {
 
 	def discoverINDs(inputs: List[String], spark: SparkSession): Unit = {
-		// TODO
+
+		import spark.implicits._
+
+
+		var tablesBuffer = new ListBuffer[DataFrame]()
+
+		var rddBuffer = new ListBuffer[RDD[(String, Set[String])]]
+		for (i <- inputs.indices) {
+			tablesBuffer += spark.read
+				.option("inferSchema", "true")
+				.option("header", "true")
+				.option("sep", ";")
+				.csv(inputs(i))
+		}
+
+		val tableList = tablesBuffer.toList
+
+		tableList.foreach(df => df.columns.foreach(col =>
+			rddBuffer += df.select(col).dropDuplicates().rdd.map(r => (r(0).toString, Set[String](col)))
+		))
+
+
+		//create one rdd containing the tuples
+		val rdd = rddBuffer.toList.reduce(_ union _)
+
+		//create attributeSets
+		val attributeSets = rdd.reduceByKey(_ ++ _).values.distinct()
+
+		//create inclusionLists
+		val inclusionLists = attributeSets.flatMap(set => set.map(attribute => (attribute, set - attribute)))
+
+		//intersect inclusionLists and remove empty intersects
+		val aggregateSets = inclusionLists.reduceByKey(_.intersect(_)).filter(_._2.nonEmpty).sortByKey(numPartitions = 1)
+
+		//print the INDs
+		aggregateSets.foreach(row => {
+			println(row._1 + " < " + row._2.mkString(", "))
+		})
 	}
 }
